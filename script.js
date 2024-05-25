277a0fa9e64dd3a19c858b56e4e77cb3ad4424df
josephnwankwo <josephinwankwo@gmail.com>
2024-05-25 11:45:45 +0100

    javascript is killing me rn

--- script.js
+++ script.js
@@ -2,27 +2,60 @@
 const generatePassword = document.getElementById("btn");
 const copyText = document.getElementById("copytext");
 const password = document.getElementById("password");
+const lengthOfPassword = document.getElementById("passwordLength");
+
+// Checkbox
+const selectUppercase = document.getElementById("checkbox-uppercase");
+const selectlowerCase = document.getElementById("checkbox-lowercase");
+const selectDigits = document.getElementById("checkbox-digits");
+const selectSpecials = document.getElementById("checkbox-specials");
 
 // Generate password when the user clicks on generatePassword
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const digits = "0123456789";
 const specials = "~!@#$%^&*()_+:|<>?";
-const length = 8;
-const allCharacters = upperCase + lowerCase + digits + specials;
+const length = 10;
+const allCharacters = [];
+
+// lengthOfPassword.addEventListener("input", function (event) {
+//   let generatedPassword = "";
+//   const input = lengthOfPassword.value;
+
+//   while (input > generatedPassword.length) {
+//     generatedPassword +=
+//       allCharacters[Math.floor(Math.random() * allCharacters.length)];
+//   }
+
+//   password.value = generatedPassword;
+// });
 
 generatePassword.addEventListener("click", function () {
   let generatedPassword = "";
-  generatedPassword += upperCase[Math.floor(Math.random() * upperCase.length)];
-  generatedPassword += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  generatedPassword += digits[Math.floor(Math.random() * digits.length)];
-  generatedPassword += specials[Math.floor(Math.random() * specials.length)];
 
   while (length > generatedPassword.length) {
     generatedPassword +=
       allCharacters[Math.floor(Math.random() * allCharacters.length)];
   }
+
   password.value = generatedPassword;
+
+  if (selectUppercase.checked) {
+    allCharacters.unshift(upperCase);
+    alert("checked");
+  } else if (selectlowerCase.checked) {
+    allCharacters.push(lowerCase);
+    alert("checkeds");
+  } else if (selectDigits.checked) {
+    allCharacters.push(digits);
+    alert("checke");
+  } else if (selectSpecials.checked) {
+    allCharacters.push(specials);
+    alert("checkedss");
+  } else {
+    alert("Please select an input to get an output");
+  }
+  console.log(generatedPassword, selectUppercase, allCharacters);
 });
 
 // Copy password after it has been generated.
